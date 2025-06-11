7ba5900285ff4dda452f1fc7108bc886ba90dd67
SantiagoFlorido <santiflorido.1016@gmail.com>
2025-06-11 13:37:26 -0500

    Create Request Header Parser Microservice

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+var express = require('express');
+var app = express();
+var cors = require('cors');
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+// ✅ NUEVA RUTA PARA LA ACTIVIDAD
+app.get('/api/whoami', function (req, res) {
+  const ip = req.ip || req.connection.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ip,
+    language: language,
+    software: software
+  });
+});
+
+// Puerto
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
